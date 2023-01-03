79792517c96bb1091199c964bc677d550fad89dd
Shanbashir1 <shanbashir1@hotmail.com>
2023-01-03 19:54:38 +0000

    Add runtime.txt for heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('shan123_waniah')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
